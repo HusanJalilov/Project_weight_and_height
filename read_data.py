5036ab43f58d11f564ffa8675ced2379d07cae08
coder_uz <husanjalilov00777@gmil.com>
2022-03-11 20:21:55 +0500

    read data task

--- read_data.py
+++ read_data.py
@@ -8,7 +8,14 @@ def read_csv_data(file_path):
     Returns:
         list: data split row
     """
-    result_data = []
+    #result_data =file_path.split(/n)
+    print(type(file_path))
+    result_data=file_path.split('\n')
+    
+    
     # WRITE YOUR CODE HERE
     return result_data
+file_path=open('data/weight-height.csv').read()
+print(read_csv_data(file_path))
+
     
